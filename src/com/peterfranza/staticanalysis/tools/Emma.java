c7f178d1c8d25660e204b26f16a682551a677a13
pfranza <pfranza@e4d2af4a-b730-11dd-b55b-2da8d274f894>
2009-07-09 12:25:17 +0000

    adding jars for junit and emma although they are not implemented yet

---
+++ src/com/peterfranza/staticanalysis/tools/Emma.java
@@ -0,0 +1,18 @@
+package com.peterfranza.staticanalysis.tools;
+
+import java.util.List;
+
+import org.apache.tools.ant.Project;
+
+import com.peterfranza.staticanalysis.Analysis;
+import com.peterfranza.staticanalysis.AnalysisItem.AnalysisHolder;
+
+public class Emma extends AbstractAnalysisTool {
+
+	public void analyze(Analysis analysis, Project project,
+			List<AnalysisHolder> items) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
